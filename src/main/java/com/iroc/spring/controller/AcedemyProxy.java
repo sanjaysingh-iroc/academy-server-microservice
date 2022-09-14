a2549ed0916add0209727d6cbdea8e79d76bdf22
sam <sam.iet2006@gmail.com>
2022-09-14 21:08:34 +0530

    Enable Load Balance Client

--- src/main/java/com/iroc/spring/controller/AcedemyProxy.java
+++ src/main/java/com/iroc/spring/controller/AcedemyProxy.java
@@ -1,10 +1,12 @@
 package com.iroc.spring.controller;
 
 import com.iroc.spring.entity.AcedemyDetails;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name="academyMicroservice", url="localhost:8081")
+@FeignClient(name="academyMicroservice")
+@LoadBalancerClient(name="academyMicroservice", configuration = LoadBalanceConfiguration.class)
 public interface AcedemyProxy {
 
     @GetMapping(value = "/acedemy/getAcedemyDetails")
